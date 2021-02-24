bcd5fc560e0d3d01a259d1e586698d42e3feba74
Diego <diegoteixeirapereira@hotmail.com>
2021-02-24 08:58:17 -0300

    criação da classe principal do projeto

---
+++ javaEscreverTXT/src/br/com/java/Main.java
@@ -0,0 +1,34 @@
+package br.com.java;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		Scanner ler = new Scanner(System.in);
+		int i, n;
+		
+		System.out.printf("Informe o número para a tabuada:\n");
+		n = ler.nextInt();
+		
+		FileWriter arq = new FileWriter("C:\\Users\\administrator\\Documents\\tabuada.txt");
+		PrintWriter gravarArq = new PrintWriter(arq);
+		
+		gravarArq.printf("+--Resultado--+%n");
+		
+		for (i = 1; i <=10; i++) {
+			gravarArq.printf("| %2d X %d = %2d |%n", i, n, (i*n));
+		}
+		gravarArq.printf("+-------+%n");
+		
+		arq.close();
+		
+		System.out.printf("\nTabuada do %d foi gravada com sucesso em \"C:\\Users\\administrator\\Documentos\\tabuada.txt\".\n",n);
+
+	}
+
+}
